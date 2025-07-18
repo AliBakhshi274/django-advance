3c4f63116673983fb2450c13ee5b0d96bbdfcdf8
AliBakhshi274 <alibakhshi.bs@gmail.com>
2025-07-18 09:48:02 +0330

    build post- and category-  models and their relationship

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    status = models.BooleanField()
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
