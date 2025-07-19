be8e6f2594bcc9443f7f2f102705c29ed5a94217
AliBakhshi274 <alibakhshi.bs@gmail.com>
2025-07-19 22:59:29 +0330

    blog app is added and created its models (Post and Category)

--- core/blog/admin.py
+++ core/blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Post
 
-# Register your models here.
+# Register Post model.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+    list_display = ('title', 'category', 'status')
+    list_filter = ('status', 'category')
+    ordering = ('category',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    model= Category
--- core/blog/models.py
+++ core/blog/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 # Create your models here.
 class Post(models.Model):
-    # author = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
     image = models.ImageField(null=True, blank=True)
     title = models.CharField(max_length=150)
     content = models.TextField()
